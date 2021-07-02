125b7f540cadc7bc84acd2f21e9fc8e7e8e17541
UncertainProd <uncertainprod31415@gmail.com>
2021-07-02 20:13:34 +0530

    One last ditch effort to make any errors visible to the user

--- xmlpngengine.py
+++ xmlpngengine.py
@@ -23,109 +23,114 @@ def add_pose_numbers(pose_arr:list[str]):
     return new_pose_arr
 
 def make_png_xml(imgpaths:list[str], pose_names:list[str], save_dir:str, character_name:str="Result", clip=False):
-    # PNG stuff
-    widths = []
-    heights = []
-    exceptionmsg = None
-    for impath in imgpaths:
-        try:
-            im = Image.open(impath)
-        except Exception as e:
-            exceptionmsg = str(e)
-            print("Error: ", exceptionmsg)
-            return 1, exceptionmsg
-        else:
-            if not clip:
-                widths.append(im.width + 4) # 2 pixels padding on each side
-                heights.append(im.height + 4)
-            else:
-                box = im.getbbox()
-                widths.append(box[2] - box[0] + 4)
-                heights.append(box[3] - box[1] + 4)
-            im.close()
-    row_width_sums = []
-    for i in range(0, len(widths), 4):
-        row_width_sums.append(sum(widths[i:i+4]))
-    final_img_width = max(row_width_sums)
-
-    max_heights = []
-    for i in range(0, len(heights), 4):
-        max_heights.append(max(heights[i:i+4]))
-    final_img_height = sum(max_heights)
-
-    # XML Stuff
-    root = ET.Element("TextureAtlas")
-    root.tail = '\n' # os.linesep
-    root.attrib['imagePath'] = character_name + ".png"
-
-    final_img = Image.new('RGBA', (final_img_width, final_img_height), color=(0, 0, 0, 0))
-    print("Final image size: ({}, {})".format(final_img_width, final_img_height))
-    num_cols = 4
-    csx = csy = 0
-    newPoseNames = add_pose_numbers(pose_names)
-    for i, imgpath in enumerate(imgpaths):
-        print("Adding {} to final_image...".format(imgpath))
-        try:
-            old_img = Image.open(imgpath)
-        except Exception as e:
-            exceptionmsg = str(e)
-            return 1, exceptionmsg
-        else:
-            new_img = pad_img(old_img, clip)
-
-            row = i // num_cols
-            col = i % num_cols
-
-            if col == 0:
-                csx = 0
-            csy = sum(max_heights[:row])
-            
-            subtexture_element = ET.Element("SubTexture")
-            subtexture_element.tail = '\n' # os.linesep
-            subtexture_element.attrib = {
-                "name" : character_name + " " + newPoseNames[i],
-                "x": f'{csx}',
-                "y": f'{csy}',
-                "width": f'{new_img.width}',
-                "height": f'{new_img.height}',
-                "frameX": '0',
-                "frameY": '0',
-                "frameWidth": f'{new_img.width}',
-                "frameHeight": f'{new_img.height}',
-            }
-            root.append(subtexture_element)
-
-            final_img.paste(new_img, (csx, csy))
-            
-            csx += new_img.width
-            
-            old_img.close()
-            new_img.close()
-
-    # Saving png
-    print(f"Saving final image....")
-    # final_img.save(os.path.join(save_dir, character_name) + ".png")
     try:
-        final_img.save(save_dir + '\\' + character_name + ".png")
+        # PNG stuff
+        widths = []
+        heights = []
+        exceptionmsg = None
+        for impath in imgpaths:
+            try:
+                im = Image.open(impath)
+            except Exception as e:
+                exceptionmsg = str(e)
+                print("Error: ", exceptionmsg)
+                return 1, exceptionmsg
+            else:
+                if not clip:
+                    widths.append(im.width + 4) # 2 pixels padding on each side
+                    heights.append(im.height + 4)
+                else:
+                    box = im.getbbox()
+                    widths.append(box[2] - box[0] + 4)
+                    heights.append(box[3] - box[1] + 4)
+                im.close()
+        row_width_sums = []
+        for i in range(0, len(widths), 4):
+            row_width_sums.append(sum(widths[i:i+4]))
+        final_img_width = max(row_width_sums)
+
+        max_heights = []
+        for i in range(0, len(heights), 4):
+            max_heights.append(max(heights[i:i+4]))
+        final_img_height = sum(max_heights)
+
+        # XML Stuff
+        root = ET.Element("TextureAtlas")
+        root.tail = '\n' # os.linesep
+        root.attrib['imagePath'] = character_name + ".png"
+
+        final_img = Image.new('RGBA', (final_img_width, final_img_height), color=(0, 0, 0, 0))
+        print("Final image size: ({}, {})".format(final_img_width, final_img_height))
+        num_cols = 4
+        csx = csy = 0
+        newPoseNames = add_pose_numbers(pose_names)
+        for i, imgpath in enumerate(imgpaths):
+            print("Adding {} to final_image...".format(imgpath))
+            try:
+                old_img = Image.open(imgpath)
+            except Exception as e:
+                exceptionmsg = str(e)
+                return 1, exceptionmsg
+            else:
+                new_img = pad_img(old_img, clip)
+
+                row = i // num_cols
+                col = i % num_cols
+
+                if col == 0:
+                    csx = 0
+                csy = sum(max_heights[:row])
+                
+                subtexture_element = ET.Element("SubTexture")
+                subtexture_element.tail = '\n' # os.linesep
+                subtexture_element.attrib = {
+                    "name" : character_name + " " + newPoseNames[i],
+                    "x": f'{csx}',
+                    "y": f'{csy}',
+                    "width": f'{new_img.width}',
+                    "height": f'{new_img.height}',
+                    "frameX": '0',
+                    "frameY": '0',
+                    "frameWidth": f'{new_img.width}',
+                    "frameHeight": f'{new_img.height}',
+                }
+                root.append(subtexture_element)
+
+                final_img.paste(new_img, (csx, csy))
+                
+                csx += new_img.width
+                
+                old_img.close()
+                new_img.close()
+
+        # Saving png
+        print(f"Saving final image....")
+        # final_img.save(os.path.join(save_dir, character_name) + ".png")
+        try:
+            final_img.save(save_dir + '\\' + character_name + ".png")
+        except Exception as e:
+            exceptionmsg = str(e)
+            return 1, exceptionmsg
+        else:
+            final_img.close()
+
+        # Saving XML
+        print("Saving XML")
+        xmltree = ET.ElementTree(root)
+        # with open(os.path.join(save_dir, character_name) + ".xml", 'wb') as f:
+        try:
+            with open(save_dir + '\\' + character_name + ".xml", 'wb') as f:
+                xmltree.write(f, xml_declaration=True, encoding='utf-8')
+            print("Done!")
+        except Exception as e:
+            exceptionmsg = str(e)
+            return 1, exceptionmsg
+    
     except Exception as e:
         exceptionmsg = str(e)
         return 1, exceptionmsg
     else:
-        final_img.close()
-
-    # Saving XML
-    print("Saving XML")
-    xmltree = ET.ElementTree(root)
-    # with open(os.path.join(save_dir, character_name) + ".xml", 'wb') as f:
-    try:
-        with open(save_dir + '\\' + character_name + ".xml", 'wb') as f:
-            xmltree.write(f, xml_declaration=True, encoding='utf-8')
-        print("Done!")
-    except Exception as e:
-        exceptionmsg = str(e)
-        return 1, exceptionmsg
-    
-    return 0, None
+        return 0, None
 
 def clean_up(*args):
     for img in args:
