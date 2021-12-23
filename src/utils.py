b3d73c616fccac10f1099ad4e225254829935c21
UncertainProd <ucprod@github.com>
2021-12-23 23:20:29 +0530

    Removed some redundancies from utis

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,4 @@
 from PyQt5.QtWidgets import QMessageBox
-from PIL import Image
 
 SPRITEFRAME_SIZE = 128
 imghashes = {} # dict[Int(hash) -> PIL.Image object]
@@ -39,35 +38,6 @@ def parse_value(val, exceptions=None, fallback=0, dtype=int):
             print("Could not convert into required type")
             print(e)
             return fallback
-
-
-# def get_true_frame(img , framex, framey, framew, frameh):
-#     # if framex < 0, we pad, else we crop
-#     final_frame = img
-#     if framex < 0:
-#         final_frame = pad_img(final_frame, False, 0, 0, 0, -framex)
-#     else:
-#         final_frame = final_frame.crop((framex, 0, final_frame.width, final_frame.height))
-    
-#     # same for framey
-#     if framey < 0:
-#         final_frame = pad_img(final_frame, False, -framey, 0, 0, 0)
-#     else:
-#         final_frame = final_frame.crop((0, framey, final_frame.width, final_frame.height))
-    
-#     # if framex + framew > img.width, we pad else we crop
-#     if framex + framew > img.width:
-#         final_frame = pad_img(final_frame, False, 0, framex+framew - img.width, 0, 0)
-#     else:
-#         final_frame = final_frame.crop((0, 0, framex+framew, final_frame.height))
-    
-#     # same for framey + frameh > img.height
-#     if framey + frameh > img.height:
-#         final_frame = pad_img(final_frame, False, 0, 0, framey + frameh - img.height, 0)
-#     else:
-#         final_frame = final_frame.crop((0, 0, final_frame.width, framey+frameh))
-    
-#     return final_frame
 
 if __name__ == '__main__':
     print("To run the actual application, Please type: \npython xmlpngUI.py\nor \npython3 xmlpngUI.py \ndepending on what works")
